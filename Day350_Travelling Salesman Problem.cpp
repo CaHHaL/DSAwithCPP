9835e7eafaa950b5df694088d53704c9bfd11f62
Cahal Agarwalla <144279003+CaHHaL@users.noreply.github.com>
2025-12-03 21:27:34 +0530

    added

---
+++ Day350_Travelling Salesman Problem.cpp
@@ -0,0 +1,44 @@
+// Given a 2d matrix cost[][] of size n where cost[i][j] denotes the cost of moving from city i to city j. Your task is to complete a tour from city 0 (0-based index) to all other cities such that you visit each city exactly once and then at the end come back to city 0 at minimum cost.
+
+// Examples:
+
+// Input: cost[][] = [[0, 111],
+//                 [112, 0]]
+// Output: 223
+// Explanation: We can visit 0->1->0 and cost = 111 + 112.
+// Input: cost[][] = [[0, 1000, 5000],
+//                 [5000, 0, 1000],
+//                 [1000, 5000, 0]]
+// Output: 3000
+// Explanation: We can visit 0->1->2->0 and cost = 1000 + 1000 + 1000 = 3000
+class Solution
+{
+public:
+    int solve(vector<vector<int>> &cost, int mask, int pos, vector<vector<int>> &dp)
+    {
+        int n = cost.size();
+        if (mask == (1 << n) - 1)
+        {
+            return cost[pos][0];
+        }
+        if (dp[mask][pos] != -1)
+            return dp[mask][pos];
+        int ans = 1e9;
+        for (int i = 0; i < n; i++)
+        {
+            if (!((1 << i) & mask))
+            {
+                int num = cost[pos][i] + solve(cost, (mask | (1 << i)), i, dp);
+                ans = min(ans, num);
+            }
+        }
+        return dp[mask][pos] = ans;
+    }
+    int tsp(vector<vector<int>> &cost)
+    {
+        int n = cost.size();
+        vector<vector<int>> dp(1 << n, vector<int>(n + 1, -1));
+        int ans = solve(cost, 1, 0, dp);
+        return ans;
+    }
+};
